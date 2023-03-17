ab9ab2004004a3a2de15732a30d9324cd027f7ff
Dmitry <divinedavidbrown@yandex.ru>
2023-03-17 18:28:49 +0300

    add func to set the initial view display

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    private func rounded(view: UIView) {
+        view.layer.cornerRadius = 75
+    }
+    
+    private func changeOfBrightness(view: UIView) {
+        view.alpha = 0.3
+    }
+    
+    private func setupView() {
+        rounded(view: yellowLight)
+        rounded(view: redLight)
+        rounded(view: greenLight)
+        changeOfBrightness(view: yellowLight)
+        changeOfBrightness(view: redLight)
+        changeOfBrightness(view: greenLight)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        nextButton.layer.cornerRadius = 20
+        
     }
-
-
+    
+    @IBAction func switcLights(_ sender: UIButton) {
+        
+    }
 }
-
